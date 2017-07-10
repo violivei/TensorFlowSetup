d8d317c9a8e76cae61ffd5700fefd6cc4995e0dd
violivei <voa@cin.ufpe.br>
2017-07-10 15:12:43 +0100

    adjustments

--- install-tensorflow.sh
+++ install-tensorflow.sh
@@ -1,20 +1,20 @@
-# # Installing dependencies & build tools
-# # First we need to install the dependencies and basic build tools required by TensorFlow. 
-# # To do this execute the following commands from the ubuntu command prompt.
-# sudo apt-get update
-# sudo apt-get upgrade -y # choose “install package maintainers version”
-# sudo apt-get install -y build-essential git python-pip libfreetype6-dev libxft-dev libncurses-dev libopenblas-dev gfortran python-matplotlib libblas-dev liblapack-dev libatlas-base-dev python-dev python-pydot linux-headers-generic linux-image-extra-virtual unzip python-numpy swig python-pandas python-sklearn unzip wget pkg-config zip g++ zlib1g-dev libcurl3-dev
-# sudo pip install -U pip
+# Installing dependencies & build tools
+# First we need to install the dependencies and basic build tools required by TensorFlow. 
+# To do this execute the following commands from the ubuntu command prompt.
+sudo apt-get update
+sudo apt-get upgrade -y # choose “install package maintainers version”
+sudo apt-get install -y build-essential git python-pip libfreetype6-dev libxft-dev libncurses-dev libopenblas-dev gfortran python-matplotlib libblas-dev liblapack-dev libatlas-base-dev python-dev python-pydot linux-headers-generic linux-image-extra-virtual unzip python-numpy swig python-pandas python-sklearn unzip wget pkg-config zip g++ zlib1g-dev libcurl3-dev
+sudo pip install -U pip
 
-# # Installing CUDA 8
-# # Second we wish to use the latest version of the CUDA library with the instance type. 
-# # The latest version at the date of writing this post is 8.0. 
-# # To install CUDA download the package from NVIDIA and install it.
-# wget https://developer.nvidia.com/compute/cuda/8.0/prod/local_installers/cuda-repo-ubuntu1604-8-0-local_8.0.44-1_amd64-deb
-# sudo dpkg -i cuda-repo-ubuntu1604-8-0-local_8.0.44-1_amd64-deb
-# rm cuda-repo-ubuntu1604-8-0-local_8.0.44-1_amd64-deb
-# sudo apt-get update
-# sudo apt-get install -y cuda
+# Installing CUDA 8
+# Second we wish to use the latest version of the CUDA library with the instance type. 
+# The latest version at the date of writing this post is 8.0. 
+# To install CUDA download the package from NVIDIA and install it.
+wget https://developer.nvidia.com/compute/cuda/8.0/prod/local_installers/cuda-repo-ubuntu1604-8-0-local_8.0.44-1_amd64-deb
+sudo dpkg -i cuda-repo-ubuntu1604-8-0-local_8.0.44-1_amd64-deb
+rm cuda-repo-ubuntu1604-8-0-local_8.0.44-1_amd64-deb
+sudo apt-get update
+sudo apt-get install -y cuda
 
 # Configure the Environment
 # Finally we need to configure the environment to work with CUDA and cuDNN. 
